35faaefd0a049890d2e3022f40a720f44c583406
sozdatel31 <pavlik.gerasim@gmail.com>
2021-04-05 23:01:43 +0300

    completed 5 task for Ignat my header is BAD(((

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,27 +1,37 @@
 import React from 'react'
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+import {Redirect, Route, Switch } from 'react-router-dom';
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/Junior+";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
-    // add paths
+    JUNIOR: '/junior',
+    JUNIOR_PlUS: '/juniorPlus'
 }
 
 function Routes() {
     return (
         <div>
-            {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            {/*//Switch выбирает первый подходящий роут*/}
+            <Switch>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
+            <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+            <Route path={'/'} exact render={() => <Redirect to={PATH.JUNIOR}/>}/>
+            <Route path={'/'} exact render={() => <Redirect to={PATH.JUNIOR_PlUS}/>}/>
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
-                // add routes
+            <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
+            <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
+            <Route path={PATH.JUNIOR_PlUS} render={() => <JuniorPlus/>}/>
+            {/*    // add routes*/}
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route render={() => <Error404/>}/>
 
-            {/*</Switch>*/}
+            </Switch>
         </div>
     )
 }
