db782c3de8bbae6ea21649b8479917576005cd7b
sozdatel31 <pavlik.gerasim@gmail.com>
2021-05-08 19:47:45 +0300

    completed 10 task

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,14 +1,14 @@
-export const homeWorkReducer = (state: { _id: number, name: string, age: number }[],
-                                action: { type: string, payload: string | number }):
+export const homeWorkReducer = (
+    state: { _id: number, name: string, age: number }[],
+    action: { type: string, payload: string | number }):
     { _id: number, name: string, age: number }[] => {
     switch (action.type) {
         case "sort":
             const deeepState = [...state].sort((a, b) => a.name > b.name ? 1 : -1)
             const deeepState2 = [...state].sort((a, b) => a.name < b.name ? 1 : -1)
-            return action.payload == "up" ? deeepState : deeepState2
+            return action.payload === "up" ? deeepState : deeepState2
         case "check":
-            const deepState3 = [...state].filter(f => f.age > action.payload)
-            return deepState3
+            return [...state].filter(f => f.age > action.payload)
         default:
             return state
     }
