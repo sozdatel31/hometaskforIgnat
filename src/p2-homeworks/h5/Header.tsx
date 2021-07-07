beadca754164dc297dae9abcaa1ed43440169158
sozdatel31 <pavlik.gerasim@gmail.com>
2021-07-07 21:46:24 +0300

    fix

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import {NavLink} from "react-router-dom";
 import {PATH} from "./Routes";
 import s from "./Header.module.css"
+
 function Header() {
     return (
         <div className={s.Headder}>
 
             <div className={s.Divv}>
-                 <NavLink className={s.Navi} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
-            <NavLink className={s.Navi} to={PATH.JUNIOR}>Junior</NavLink>
-            <NavLink className={s.Navi} to={PATH.JUNIOR_PlUS}>Junior+</NavLink>
-            {/*// add NavLinks*/}
+                <NavLink className={s.Navi} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+                <NavLink className={s.Navi} to={PATH.JUNIOR}>Junior</NavLink>
+                <NavLink className={s.Navi} to={PATH.JUNIOR_PlUS}>Junior+</NavLink>
+                {/*// add NavLinks*/}
             </div>
         </div>
     )
